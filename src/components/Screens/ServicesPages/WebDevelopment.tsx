90acbf125cdb40d6dd101c0799f7bc77d5baa60f
Ansh Goyal <anshgoyal1704@gmail.com>
2023-06-22 14:49:54 +0530

    add web-development page

--- src/components/Screens/ServicesPages/WebDevelopment.tsx
+++ src/components/Screens/ServicesPages/WebDevelopment.tsx
@@ -3,14 +3,32 @@ import { WebDevelopmentHero } from "../../Fragments/ServicePagesFragments/WebDev
 import { PreviousWork } from "../../Fragments/ServicePagesFragments/WebDevelopmentPageFragment/PreviousWork";
 import { previousWorkData1 } from "./PreviousWorkData1";
 import { previousWorkData2 } from "./PreviousWorkData2";
+import { Title } from "@mantine/core";
 
 function WebDevelopmentPage() {
   return (
     <div>
       <WebDevelopmentHero />
+      <Title
+        align="center"
+        mb={50}
+        sx={(theme) => ({
+          fontWeight: 900,
+        })}
+      >
+        Some of our previous work
+      </Title>
       <PreviousWork data={previousWorkData1} />
       <PreviousWork data={previousWorkData2} />
-      Still Not Convinced?
+      <Title
+        align="center"
+        mt={50}
+        sx={(theme) => ({
+          fontWeight: 900,
+        })}
+      >
+        Still Not Convinced? Let's Talk!
+      </Title>
     </div>
   );
 }
