8b07b886b7f932589f4538da4a86b0c875520cba
Ansh Goyal <anshgoyal1704@gmail.com>
2023-06-23 18:00:26 +0530

    add llp sp op pages

--- src/components/Screens/ServicesPages/OnePersonCompany.tsx
+++ src/components/Screens/ServicesPages/OnePersonCompany.tsx
@@ -3,6 +3,8 @@ import { HeroHeading } from "../../Fragments/ServicePagesFragments/LegalServices
 import { heroHeadingData } from "../../data/LegalServicesPagesData/OnePersonCompany/heroHeadingData";
 import CustomH2 from "../../Fragments/CommonFragments/CustomH2";
 import { HowItWorks } from "../../Fragments/ServicePagesFragments/LegalServicesPagesFragments/HowItWorks";
+import { Benefits } from "../../Fragments/ServicePagesFragments/LegalServicesPagesFragments/Benefits";
+import { opBenefits } from "../../data/LegalServicesPagesData/OnePersonCompany/opBenefits";
 
 function OnePersonCompany() {
   return (
@@ -11,6 +13,7 @@ function OnePersonCompany() {
       <CustomH2 title={"How it Works?"} />
       <HowItWorks />
       <CustomH2 title={"Benefits of OPC"} />
+      <Benefits mockdata={opBenefits} />
     </div>
   );
 }
