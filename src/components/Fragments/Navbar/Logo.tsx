074088e14fb7630327ec7e0d55eab1590dc70881
Ansh Goyal <anshgoyal1704@gmail.com>
2023-06-27 15:01:55 +0530

    change logo to gif

--- src/components/Fragments/Navbar/Logo.tsx
+++ src/components/Fragments/Navbar/Logo.tsx
@@ -1,25 +1,13 @@
 import React from "react";
-import WhiteBGLogo from "./CodifyPlusLogoLightMode.png";
-import BlackBGLogo from "./CodifyPlusLogoDarkMode.png";
-import BlackBGLogoWebm from "./CodifyPlus Animated Logo Dark.webm";
-import WhiteBGLogoWebm from "./CodifyPlus Animated Logo.webm";
+import DarkGIF from "./DarkGIF.gif";
+import LightGIF from "./LightGIF.gif";
 import { useMantineTheme } from "@mantine/core";
 
 function Logo() {
   const { colorScheme } = useMantineTheme();
   return (
     <div>
-      <video autoPlay loop muted width={150}>
-        <source
-          src={colorScheme === "dark" ? BlackBGLogoWebm : WhiteBGLogoWebm}
-          type="video/webm"
-        />
-        <img
-          src={colorScheme === "dark" ? BlackBGLogo : WhiteBGLogo}
-          alt=""
-          width={150}
-        />
-      </video>
+      <img src={colorScheme === "dark" ? DarkGIF : LightGIF} alt="" width={150} />
     </div>
   );
 }
