c8fe1d16deeba94027659f0fe826e2bdc42a738e
Ansh Goyal <anshgoyal1704@gmail.com>
2023-06-30 09:15:08 +0530

    add react lazy

--- src/components/Screens/Home.tsx
+++ src/components/Screens/Home.tsx
@@ -1,23 +1,13 @@
-import React, { Suspense } from "react";
+import React from "react";
+import { Hero } from "../Fragments/HomeFragments/Hero";
+import { FeaturesSection } from "../Fragments/HomeFragments/Featured";
 import featuredData from "../data/featuredData";
-const Hero = React.lazy(() =>
-  import("../Fragments/HomeFragments/Hero").then((module) => ({
-    default: module.Hero,
-  }))
-);
-const FeaturesSection = React.lazy(() =>
-  import("../Fragments/HomeFragments/Featured").then((module) => ({
-    default: module.FeaturesSection,
-  }))
-);
 
 function Home() {
   return (
     <>
-      <Suspense fallback={<div></div>}>
-        <Hero></Hero>
-        <FeaturesSection {...featuredData} />
-      </Suspense>
+      <Hero></Hero>
+      <FeaturesSection {...featuredData} />
     </>
   );
 }
